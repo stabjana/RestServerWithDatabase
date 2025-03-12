97141bbc80b864cb508e38381d4178a948d1b026
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-03-12 13:48:52 +0200

    add sqlStatements createing new db

---
+++ storages/personSqlStorage/employeeCreateStatement.sql
@@ -0,0 +1,21 @@
+drop database if exists employeedb;
+create database employeedb;
+use employeedb;
+
+create table employee {
+    id int not null primary key,
+    firstname varchar(20) not null,
+    lastname varchar(30) not null,
+    department varchar(15) not null,
+    salary decimal (6,2)
+};
+
+insert into employee values (1, 'John', 'Doozzle', 'ICT', 50000.00);
+insert into employee values (2, 'Hans', 'Doe', 'HR', 60000.00);
+insert into employee values (3, 'Matt', 'River', 'ICT', 70000.00);
+insert into employee values (4, 'Linda', 'Blurr', 'HR', 80000.00);
+
+drop user if exists 'horst'@'localhost';
+create user 'horst'@'localhost' identified by '1234';
+grant all privileges on employeedb.* to 'horst'@'localhost';
+
